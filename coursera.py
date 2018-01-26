20e5a53200f05c43c307b92fc16ffac004b711f8
Serg4356 <asdfg111000@yandex.ru>
2018-01-27 01:12:15 +0500

    rename variables, simplify code

--- coursera.py
+++ coursera.py
@@ -7,16 +7,16 @@ import os
 import argparse
 
 
-def get_courses_link_list(courses_number, http_response):
-    etree_courses = etree.fromstring(http_response)
+def get_courses_link_list(courses_number, http_content):
+    etree_courses = etree.fromstring(http_content)
     courses_link_list = []
     for child in etree_courses.getchildren():
         courses_link_list.append(child.getchildren()[0].text)
     return courses_link_list[-courses_number:]
 
 
-def get_course_info(http_response):
-    soup = BeautifulSoup(http_response, 'html.parser')
+def get_course_info(http_content):
+    soup = BeautifulSoup(http_content, 'html.parser')
     course_info = {}
     course_info['name'] = soup.h1.text
     try:
@@ -33,15 +33,13 @@ def get_course_info(http_response):
     return course_info
 
 
-def output_courses_info_to_xls(courses_info, course_attr_names):
+def output_courses_info_to_xls(courses_info):
     courses_workbook = openpyxl.Workbook()
-    work_sheet = courses_workbook.create_sheet('coursera_courses')
-    for column, course_attr_name in enumerate(course_attr_names, start=2):
-        work_sheet.cell(row=1, column=column).value = course_attr_name
-    for row, course in enumerate(courses_info, start=2):
-        for column, key in enumerate(course.keys(), start=2):
-            work_sheet.cell(row=row, column=1).value = row
-            work_sheet.cell(row=row, column=column).value = course[key]
+    work_sheet = courses_workbook.active
+    work_sheet.title = 'coursera_courses'
+    work_sheet.append(list(courses_info[0].keys()))
+    for course in courses_info:
+        work_sheet.append(list(course.values()))
     return courses_workbook
 
 
@@ -71,26 +69,21 @@ def create_parser():
     return parser
 
 
-def output_courses_to_console(course_info, output_need):
+def output_courses_to_console(course_info, display_need):
     print('\n')
-    if output_need:
+    if display_need:
         for course_attr in course_info:
             print('{} - {}'.format(
                 course_attr,
-                course_info[course_attr])
-            )
+                course_info[course_attr]
+            ))
 
 
 if __name__ == '__main__':
     parser = create_parser()
+    excel_output = parser.parse_args().output
+    display_need = parser.parse_args().display
     courses_number = 20
-    course_attr_names = [
-        'name',
-        'average grade',
-        'weeks required',
-        'language',
-        'start'
-    ]
     coursera_links_xml = fetch_http_response(
         'https://www.coursera.org/sitemap~www~courses.xml'
     )
@@ -99,23 +92,23 @@ if __name__ == '__main__':
         coursera_links_xml
     )
     courses_info = []
-    excel_output = parser.parse_args().output
     for course in courses_link_list:
         course_page = fetch_http_response(course)
         course_info = get_course_info(course_page)
         output_courses_to_console(
             course_info,
-            excel_output
+            display_need
         )
         courses_info.append(course_info)
     try:
         courses_book = output_courses_info_to_xls(
-            courses_info,
-            course_attr_names)
+            courses_info
+        )
         courses_book.save(
             os.path.join(
-                parser.parse_args().output,
-                'courses.xls')
+                excel_output,
+                'courses.xls'
+            )
         )
     except IndexError:
         print('Error! Input path to folder.')
